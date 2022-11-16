a03a5f0fa4f440df320dfd7838c605d1dc9e3036
Hugo Olaitan <holaitan@proadv.adv.br>
2022-11-16 14:48:32 -0300

    novas rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,12 +6,4 @@ router.get('/', function (req, res, next) {
         version: "0.0.1"
     });
 });
-
-// router.get('/teste', function (req, res, next) {
-//     res.status(200).send({
-//         title: "teste",
-//         version: "0.0.2"
-//     });
-// });
-
 module.exports = router;
