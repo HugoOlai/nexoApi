1c746b9cf6c2ff33a52f4e512a11d61eb5969a59
Hugo Olaitan <holaitan@proadv.adv.br>
2022-11-16 14:59:23 -0300

    corrigindo nome

--- src/routes/personRoute.js
+++ src/routes/personRoute.js
@@ -4,4 +4,10 @@ const controller = require('../controllers/personController')
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/:id', controller.delete);
+router.get('/', function (req, res, next) {
+    res.status(200).send({
+        title: "teste",
+        version: "0.0.1"
+    });
+});
 module.exports = router;
